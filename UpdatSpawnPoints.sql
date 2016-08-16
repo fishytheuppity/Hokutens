b5f1ba2197826552cf9306493e730a4814f358a7
Durandal71 <doyle.ezell@gmail.com>
2016-08-16 15:01:35 -0400

    Add files via upload

---
+++ UpdatSpawnPoints.sql
@@ -0,0 +1,13 @@
+UPDATE `mob_spawn_points` SET pos_x=19, pos_y=16, pos_z=-140, pos_rot=0 WHERE mobid=17645809;
+UPDATE `mob_spawn_points` SET pos_x=-144, pos_y=1, pos_z=446, pos_rot=0 WHERE mobid=17629645;
+UPDATE `mob_spawn_points` SET pos_x=-659, pos_y=0, pos_z=-338, pos_rot=0 WHERE mobid=17289656;
+UPDATE `mob_spawn_points` SET pos_x=218, pos_y=-8, pos_z=205, pos_rot=0 WHERE mobid=17428817;
+UPDATE `mob_spawn_points` SET pos_x=-324, pos_y=0, pos_z=474, pos_rot=0 WHERE mobid=17273295;
+UPDATE `mob_spawn_points` SET pos_x=117, pos_y=35, pos_z=-280, pos_rot=0 WHERE mobid=17433006;
+UPDATE `mob_spawn_points` SET pos_x=107, pos_y=0, pos_z=-125, pos_rot=0 WHERE mobid=17498565;
+UPDATE `mob_spawn_points` SET pos_x=110, pos_y=14, pos_z=161, pos_rot=0 WHERE mobid=17649861;
+UPDATE `mob_spawn_points` SET pos_x=201, pos_y=11, pos_z=99, pos_rot=0 WHERE mobid=17490235;
+UPDATE `mob_spawn_points` SET pos_x=197, pos_y=-8, pos_z=-27, pos_rot=0 WHERE mobid=17277126;
+UPDATE `mob_spawn_points` SET pos_x=119, pos_y=20, pos_z=143, pos_rot=0 WHERE mobid=17617165;
+UPDATE `mob_spawn_points` SET pos_x=-156, pos_y=-8, pos_z=198, pos_rot=0 WHERE mobid=17240424;
+UPDATE `mob_spawn_points` SET pos_x=-11, pos_y=-19, pos_z=-176, pos_rot=0 WHERE mobid=17404338;
