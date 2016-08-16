07d840b8a61c034d68eba914f03f1d1cad8902eb
Mizzguy <Mizzguy@users.noreply.github.com>
2016-08-16 18:17:01 -0400

    Update Stolas.lua

--- Scripts/zones/Cape_Terrigan/mobs/Stolas.lua
+++ Scripts/zones/Cape_Terrigan/mobs/Stolas.lua
@@ -26,8 +26,8 @@ end;
 
 function onMobDeath(mob, player, isKiller)
     if (player:getVar("Weaponskill_Bow") == 1) and (player:hasKeyItem(MAP_TO_THE_ANNALS_OF_TRUTH) == true) then
-	   player:addKeyItem(ANNALS_OF_TRUTH);
-	   player:delKeyItem(MAP_TO_THE_ANNALS_OF_TRUTH);
-	   player:setVar("Weaponskill_Bow",2);
-	end   
+	player:addKeyItem(ANNALS_OF_TRUTH);
+	player:delKeyItem(MAP_TO_THE_ANNALS_OF_TRUTH);
+	player:setVar("Weaponskill_Bow",2);
+   end   
 end;
