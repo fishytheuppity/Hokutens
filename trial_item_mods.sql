ba459f8977b3a0b8df1e03d40b0f1bfac40e37eb
Durandal71 <doyle.ezell@gmail.com>
2016-08-16 15:22:37 -0400

    Add Weaponskill to Trial Weapons

---
+++ trial_item_mods.sql
@@ -0,0 +1,13 @@
+INSERT INTO `item_mods` VALUES (17507,355,9); -- Asuran Fists
+INSERT INTO `item_mods` VALUES (17654,355,42); -- Savage Blade
+INSERT INTO `item_mods` VALUES (16952,355,56); -- Ground Strike
+INSERT INTO `item_mods` VALUES (17933,355,72); -- Decimation
+INSERT INTO `item_mods` VALUES (16735,355,88); -- Steel Cyclone
+INSERT INTO `item_mods` VALUES (16793,355,104); -- Spiral Hell
+INSERT INTO `item_mods` VALUES (16892,355,120); -- Impulse Drive
+INSERT INTO `item_mods` VALUES (17773,355,136); -- Blade: Ku
+INSERT INTO `item_mods` VALUES (17815,355,152); -- Tachi: Kasha
+INSERT INTO `item_mods` VALUES (17456,355,169); -- Black Halo
+INSERT INTO `item_mods` VALUES (17527,355,184); -- Retribution
+INSERT INTO `item_mods` VALUES (18144,355,199); -- Empyreal Arrow
+INSERT INTO `item_mods` VALUES (18146,355,215); -- Detonator
