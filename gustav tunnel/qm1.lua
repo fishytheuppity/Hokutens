c7b75ba884a6939c944db9501f884d94ddb51560
Mizzguy <Mizzguy@users.noreply.github.com>
2016-08-16 11:24:41 -0400

    Add files via upload
    for ws quest

---
+++ gustav tunnel/qm1.lua
@@ -0,0 +1,48 @@
+-----------------------------------
+-- Area: Gustav Tunnel
+-- NPC:  qm?(???)
+-- Spawns Baronial Bat
+-- @pos -1 -100 -1 0
+-----------------------------------
+package.loaded["scripts/zones/Gustav_Tunnel/TextIDs"] = nil;
+-----------------------------------
+require("scripts/zones/Gustav_Tunnel/TextIDs");
+require("scripts/globals/settings");
+require("scripts/globals/status");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+    if (player:hasKeyItem(MAP_TO_THE_ANNALS_OF_TRUTH)) then
+	   SpawnMob(17645809); --Baronial Bat
+	else   
+       player:messageSpecial(NOTHING_OUT_OF_ORDINARY);
+	end   
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+    -- printf("CSID: %u",csid);
+    -- printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+    -- printf("CSID: %u",csid);
+    -- printf("RESULT: %u",option);
+end;
