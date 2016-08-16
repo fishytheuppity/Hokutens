c5d2b6f76bd103b4dd60c0e46a9727ffc84c02f1
Mizzguy <Mizzguy@users.noreply.github.com>
2016-08-16 18:20:17 -0400

    Update qm1.lua

--- Scripts/zones/Gustav_Tunnel/npcs/qm1.lua
+++ Scripts/zones/Gustav_Tunnel/npcs/qm1.lua
@@ -23,10 +23,10 @@ end;
 
 function onTrigger(player,npc)
     if (player:getVar("Weaponskill_Dagger") == 1) and (player:hasKeyItem(MAP_TO_THE_ANNALS_OF_TRUTH)) then
-	   SpawnMob(17645809):updateClaim(player); --Baronial Bat
-	else   
+	SpawnMob(17645809):updateClaim(player); --Baronial Bat
+    else   
        player:messageSpecial(NOTHING_OUT_OF_ORDINARY);
-	end   
+    end   
 end;
 
 -----------------------------------
